c6f0fbcb87c324bf76f102328fc2b9e8bf22909a
albiremo <albertoremigi92@gmail.com>
2021-10-14 16:47:47 +0200

    major refactoring

--- dualGPy.py
+++ dualGPy.py
@@ -1,111 +1,107 @@
 import meshio
+import mypy
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
+class Mesh() :
+    def __init__(self, mesh):
+        self.mesh = mesh
+        self.graph = {}
+        self.cells = []  
+        self.boundary_cells = []
+        self.boundary_faces = []
+        self.centers = []
+        #TODO: implement in setup_mesh the method to fill it
+        self.volume = []
+        self.area = []
 
-# WIP
-def get_area(points):
-    """Returns the area of a polygon given the vertices.
-    Parameters:
-        points:     numpy.ndarray
-            Vertices of the polygon
-    Warning: the points need to be ordered clockwise or anticlockwise."""
-
-    # shift all the points by one
-    shifted = np.roll(points, 1, axis=0)
-
-    # Use the shoelace formula
-    area = 0.5 * np.sum((shifted[:, 0] + points[:, 0])*(shifted[:, 1] - points[:, 1]))
-
-    return np.abs(area)
-
-
-def get_dual_points(compliant_cells, index):
-    """Returns the points of the dual mesh nearest to the point in the mesh given by the index.
-    Parameters:
-        mesh:       meshio.Mesh object
-            Input mesh.
-        index:      int
-            Index of the point in the input mesh for which to calculate the nearest points of the dual mesh.
-    """
-    # Find the cells where the given index appears, REMEMBER the where statement gives you immediately back the indexof the compliant cell
-    # building the compliant cells list
-    _compliant = []
-    for i in range(len(compliant_cells)):
-    # compress with the use of any to determine the compliant cells
-      if any(compliant_cells[i]==index):
-        _compliant.append(i) 
-    # Find the centers of all the cells
-    return  _compliant
-
-def compute_cell_centers(mesh,cells):
-    """Returns the center points of the mesh elements, given the list of the different elements that are
-   given as ordered vectors of points.
-    Parameters:
-        cells:       list of arrays
-            list of arrays of point representing the cells. The cell Id is the position in the list.
-    """
-    x_centerpoint=0
-    y_centerpoint=0
-    #initialization of the vector of all the centerpoints of the cells
-    centro=[]
-    for elemento in cells:
-        # cycle on the point of the elements
-        for i in range(len(elemento)):
-        # compute the centerpoint (accumulating)
-         x_centerpoint += mesh.points[elemento[i],0]
-         y_centerpoint += mesh.points[elemento[i],1]
-         # define the center point
-        x_centerpoint/=len(elemento)
-        y_centerpoint/=len(elemento)
-      # append to the centerpoints vector the element computed
-        centro.append([x_centerpoint,y_centerpoint])
-      # re-initialize the accumulation vectors
+    def setup_mesh(self):
+        """ Method that setup the elements to elaborate the graph and hence casts all the cells in a vector starting from a meshio.Mesh object. It fills the cells list of array and the boundary list. """
+        for i in range(len(self.mesh.cells)):
+          for j in range(len(self.mesh.cells[i][1])):
+           self.cells.append(self.mesh.cells[i][1][j])
+    # to parse the boundaries we make use of the dictionary iterators
+        for key, value in self.mesh.cell_sets.items():
+          for key1,value1 in self.mesh.cell_sets[key].items():
+             for i in range(len(value1)):
+               self.boundary_faces.append(value1[i])
         x_centerpoint=0
         y_centerpoint=0
-    return(centro) 
+        #initialization of the vector of all the centerpoints of the cells
+        for elemento in self.cells:
+           # cycle on the point of the elements
+           for i in range(len(elemento)):
+           # compute the centerpoint (accumulating)
+             x_centerpoint += self.mesh.points[elemento[i],0]
+             y_centerpoint += self.mesh.points[elemento[i],1]
+             # define the center point
+           x_centerpoint/=len(elemento)
+           y_centerpoint/=len(elemento)
+        #  append to the centerpoints vector the element computed
+           self.centers.append([x_centerpoint,y_centerpoint])
+        #  re-initialize the accumulation vectors
+           x_centerpoint=0
+           y_centerpoint=0
 
-def get_dual(mesh,  order=False):
-    """Returns the dual mesh held in a dictionary with dual["points"] giving the coordinates and
-    dual["cells"] giving the indicies of all the cells of the dual mesh.
-    Parameters:
-        mesh:       meshio.Mesh object
-            Input mesh.
-        order:      boolean
-            Whether to reorder the indices of each cell, such that they are in anticlockwise order.
-    """
+
+    def get_area(self):
+      """Returns the area of a polygon given the vertices.
+         Parameters:
+         points:     numpy.ndarray
+            Vertices of the polygon
+         Warning: the points need to be ordered clockwise or anticlockwise."""
+
+        # shift all the points by one
+        
+      points = self.mesh.points 
+      shifted = np.roll(points, 1, axis=0)
+
+        # Use the shoelace formula
+
+      area = 0.5 * np.sum((shifted[:, 0] + points[:, 0])*(shifted[:, 1] - points[:, 1]))
+      return np.abs(area)
+
+    @staticmethod    
+    def get_dual_points(compliant_cells : int, index : int) -> int:
+      """Returns the points of the dual mesh nearest to the point in the mesh given by the index.
+          Parameters:
+          mesh:       meshio.Mesh object
+              Input mesh.
+          index:      int
+              Index of the point in the input mesh for which to calculate the nearest points of the dual mesh.
+          Return:
+              compliant 
+        # Find the cells where the given index appears, REMEMBER the where statement gives you immediately back the indexof the compliant cell
+        # building the compliant cells list
+      """       
+      compliant=[]
+      for i in range(len(compliant_cells)):
+      # compress with the use of any to determine the compliant cells
+         if any(compliant_cells[i]==index):
+           compliant.append(i) 
+      # Find the centers of all the cells
+      return compliant
     
-    assert isinstance(mesh, meshio.Mesh)
-    # parce cells and boundaries
-    #the alpha list is the list of cells (joined all the types, a todo should be to consider cells of different 
-    #types
-    alpha = []
-    beta = []
-    for i in range(len(mesh.cells)):
-       for j in range(len(mesh.cells[i][1])):
-        	alpha.append(mesh.cells[i][1][j])
-    # to parse the boundaries we make use of the dictionary iterators
-    for key, value in mesh.cell_sets.items():
-        for key1,value1 in mesh.cell_sets[key].items():
-             for i in range(len(value1)):
-               beta.append(value1[i])
-
-    print(alpha)
-    print(beta)
-    # create a dictionary for the graph
-    graph ={}
-    # Get the first set of points of the dual mesh
-    compliant_cells = get_dual_points(alpha, 0)    
-   # define a new key of the dictionary for each cell of the mesh
-    graph={}
-    for i in range(len(alpha)):
-     graph.update({i :[]})
-    # cycle on the points
-    for idx in range(1, len(mesh.points)):
+    def get_dual(self):
+     """Returns the dual mesh held in a dictionary with dual["points"] giving the coordinates and
+     dual["cells"] giving the indicies of all the cells of the dual mesh.
+     Parameters:
+         mesh:       meshio.Mesh object
+             Input mesh.
+         order:      boolean
+             Whether to reorder the indices of each cell, such that they are in anticlockwise order.
+     """
+     self.setup_mesh()
+     # Get the first set of points of the dual mesh
+     compliant_cells = Mesh.get_dual_points(self.cells, 0)    
+     # define a new key of the dictionary for each cell of the mesh
+     for i in range(len(self.cells)):
+        self.graph.update({i :[]})
+     # cycle on the points
+     for idx in range(1, len(self.mesh.points)):
         # Get the dual mesh points for a given mesh vertex and the compliant cells to be analysed
-        compliant_cells = get_dual_points(alpha, idx)
-        print(compliant_cells)
+        compliant_cells = Mesh.get_dual_points(self.cells, idx)
         # in this part we build the graph: for each point of the mesh we have the compliant cells
         # and we cycle over the compliant cells (two nested loop, with an if that avoids to inspect the same cell)
         # me create the list inter that check the common point between two vectors (that can have also different 
@@ -115,33 +111,21 @@ def get_dual(mesh,  order=False):
         for i in compliant_cells:
           for j in compliant_cells:
              if i!=j:
-               inter = list(set(alpha[i]).intersection(alpha[j]))
-               if ((len(inter)>=2) and (j not in graph[i])):
-                 graph[i].append(j)     
-    print(graph)
-    # Define the boundary cells in a similar way we did for the graph
-    bnd = []       
-    for i in range(len(alpha)):
-    # We cycle on the boundary cells marked
-        for j in beta:
-           inter = list(set(alpha[i]).intersection(j))
-           if ((len(inter)>=2) and (i not in bnd)):
-             bnd.append(i) 
-    print(bnd)        
-    # definition of the graph from the dictionary as sugegsted by networkx
-    g = nx.Graph(graph)
-    # initialization of the figure
-    plt.figure()
-    # cycle on the elements
-    for elemento in alpha:
-      # cycle on the point of the elements
-      for i in range(len(elemento)):
-       # plot the grid
-       x_value = [mesh.points[elemento[i-1],0],mesh.points[elemento[i],0]]
-       y_value = [mesh.points[elemento[i-1],1],mesh.points[elemento[i],1]]
-       plt.plot(x_value,y_value)  
-    # compute the centerpoints
-    cp = compute_cell_centers(mesh,alpha)
-    # draw the adjacency graph
-    nx.draw(g,pos=cp, with_labels=True)
-    plt.savefig('foo.png')
+               inter = list(set(self.cells[i]).intersection(self.cells[j]))
+               if ((len(inter)>=2) and (j not in self.graph[i])):
+                 self.graph[i].append(j)     
+     # Define the boundary cells in a similar way we did for the graph
+     self.boundary_cells = []       
+     for i in range(len(self.cells)):
+     # We cycle on the boundary cells marked
+         for j in self.boundary_faces:
+            inter = list(set(self.cells[i]).intersection(j))
+            if ((len(inter)>=2) and (i not in self.boundary_cells)):
+              self.boundary_cells.append(i) 
+     # definition of the graph from the dictionary as sugegsted by networkx
+
+    def generate_graph(self) -> nx.Graph:
+     """ Generating a networkx graph element """       
+     return nx.Graph(self.graph)
+
+
--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import dualGPy as dm
 # For plotting both the mesh and dual mesh
 import matplotlib.pyplot as plt
 import numpy as np
+import networkx as nx
 # two triangles and one quad
 points = [
     [0.0, 0.0],
@@ -36,18 +37,20 @@ mesh = meshio.Mesh(
     "Boundary": {"line": [[0, 2],[2,11],[11,10],[10,9],[9,8],[8,7],[7,6],[15,6],[14,15],[13,14],[12,13],[12,0]]},
 }
 )
-#print(dir(mesh.cells))
-#print(mesh.cell_sets["Boundary"])
-for key, value in mesh.cell_sets.items():
- for key1,value1 in mesh.cell_sets[key].items():
-   print(mesh.cell_sets[key][key1])
 
-#for i in range(len(mesh.cell_sets)):
-#	print(mesh.cell_sets[i])
-
-mesh.write(
-    "foo.vtk",  # str, os.PathLike, or buffer/open file
-    # file_format="vtk",  # optional if first argument is a path; inferred from extension
-)
-dm.get_dual(mesh, order=False)
-
+Mesh1 = dm.Mesh(mesh)
+Mesh1.get_dual()
+graph = Mesh1.generate_graph()
+## initialization of the figure
+plt.figure()
+## cycle on the elements
+for elemento in Mesh1.cells:
+## cycle on the point of the elements
+ for i in range(len(elemento)):
+# # plot the grid
+  x_value = [mesh.points[elemento[i-1],0],mesh.points[elemento[i],0]]
+  y_value = [mesh.points[elemento[i-1],1],mesh.points[elemento[i],1]]
+  plt.plot(x_value,y_value)  
+#draw the adjacency graph
+nx.draw(graph,pos=Mesh1.centers, with_labels=True)
+plt.savefig('foo.png')
