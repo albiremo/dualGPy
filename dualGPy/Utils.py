686ed9d0e142f7037ba30c62e58dc0da077844f9
albiremo <Alberto.Remigi@onera.fr>
2022-01-10 18:46:37 +0100

    added numba

--- dualGPy/Utils.py
+++ dualGPy/Utils.py
@@ -1,6 +1,19 @@
 import numpy as np
 from numba import njit, prange
 
+# http://www.katrin-affolter.ch/Python/performance_of_all_and_any
+@njit
+def any_non_zero_values(elements,index):
+
+    for el in elements:
+
+        if el == index:
+
+            return True
+
+    return False
+
+
 
 
 def get_dual_points(compliant_cells : int, index : int) -> int:
@@ -15,14 +28,7 @@ def get_dual_points(compliant_cells : int, index : int) -> int:
         # Find the cells where the given index appears, REMEMBER the where statement gives you immediately back the indexof the compliant cell
         # building the compliant cells list
       """
-      compliant = [e for i,e in enumerate(compliant_cells) if e is index]
+      compliant = [i for i,e in enumerate(compliant_cells) if any_non_zero_values(e,index)]
+ 
       return compliant
 
-
-@njit(parallel=True)
-def parallel_nonzero_count(arr):
-    flattened = arr.ravel()
-    sum_ = 0
-    for i in prange(flattened.size):
-        sum_ += flattened[i] != 0
-    return sum_
