1ff8100d5e5e155bf3e527f22e84c43d2c83ae8b
albiremo <Alberto.Remigi@onera.fr>
2022-08-30 15:40:04 +0200

    simple test added for the computevolume for an Hexa

--- tests/test_Geometry.py
+++ tests/test_Geometry.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath('../'))
 from dualGPy.Utils import *
-from dualGPy.Geometry import Face3D
+from dualGPy.Geometry import Face3D,Hexa
 import numpy as np
 class Test3D:
     def test_square(self):
@@ -15,4 +15,19 @@ class Test3D:
         Face = Face3D(points)
         Face.ComputeArea()
         assert(Face.area==0.5)
+    def test_cube(self):
+        g_points =  np.array([[0.0, 0.0, 0.0],
+        [1.0, 0.0, 0.0],
+        [1.0, 1.0, 0.0],
+        [0.0, 1.0, 0.0],
+        [0.0, 0.0, 1.0],
+        [1.0, 0.0, 1.0],
+        [1.0, 1.0, 1.0],
+        [0.0, 1.0, 1.0]]
+        )
+        c_points = g_points
+        faces = [[1,5,6,2],[4,5,6,7],[3,2,6,7]]
+        cella = Hexa(c_points,g_points,faces)
+        cella.ComputeVolume()
+        assert(cella.volume==1)
 
